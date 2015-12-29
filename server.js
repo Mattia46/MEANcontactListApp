da64f03bce6113d787d004037ebd5577099668de
Mattia46 <mattia.assogna@gmail.com>
2015-12-29 15:21:49 +0000

    adde the index.html page and imported bootstrap and angular.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello world from server.js');
-});
 
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000);
 console.log('Server up and runnig on post 3000');

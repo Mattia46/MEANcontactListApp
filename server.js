6d42522eb1c96617944134ebd74804867a508b11
Mattia46 <mattia.assogna@gmail.com>
2015-12-29 17:06:01 +0000

    set up a DB with mongodb

--- server.js
+++ server.js
@@ -1,33 +1,20 @@
 var express = require('express');
 var app = express();
-
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/contactlist', function(req, res) {
   console.log("I received the GET request");
 
-  person1 = {
-    name: "Mattia",
-    email: "mattia@gmail.com",
-    number: "12345"
-  };
+  db.contactlist.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
 
-  person2 = {
-    name: "Maria",
-    email: "maria@gmail.com",
-    number: "12345"
-  };
-
-  person3 = {
-    name: "Mario",
-    email: "Mario@gmail.com",
-    number: "12345"
-  };
-
-  var contactlist = [person1, person2, person3];
-  res.json(contactlist);
 });
+
 
 app.listen(3000);
 console.log('Server up and runnig on post 3000');
